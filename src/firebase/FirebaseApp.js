6a7662460c6a84669cf3cb1da7c8446add092273
MasterXoiDev <minhnhat8222@gmail.com>
2024-09-30 15:03:05 +0700

    firebase CRUD

---
+++ src/firebase/FirebaseApp.js
@@ -0,0 +1,121 @@
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, onSnapshot, serverTimestamp } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from './firebase-config';
+
+const FirebaseApp = () => {
+    const colRef = collection(db,"posts")
+    // let posts = []
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [postId,setPostId] = useState()
+    const [post, setPost] = useState([])
+    useEffect(()=>{
+        // {not real time}
+        // getDocs(colRef).then((snapshot)=>{
+        //     snapshot.docs.forEach((doc)=>{
+        //         posts.push({
+        //             id: doc.id,
+        //             ...doc.data()
+        //     })
+        //     })
+        //     setPost(posts)
+        //     console.log(post);
+            
+        // }).catch((error)=>{
+        //     console.log(error);
+        // })
+        // use onSnapshot is realtime
+        onSnapshot(colRef, (snapshot) => {
+            let posts = [];
+            snapshot.docs.forEach((doc) => {
+              posts.push({
+                id: doc.id,
+                ...doc.data(),
+              });
+            });
+            setPost(posts);
+            const docRefSigle = doc(db,"posts", "lq8ctVJfpZrE8DFlStFi")
+            getDoc(docRefSigle).then((doc)=>{
+              console.log(doc.id, doc.data());
+            })
+          }) }, []);
+         
+    const handleUpdatePost = (e)=>{
+        e.preventDefault()
+        addDoc(colRef, {
+            title: title,
+            author: author,
+            create : serverTimestamp()
+        }).then(()=>{
+            console.log("success");  
+        }).catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    const handleRemovePost = async(e)=>{
+        e.preventDefault()
+        const conRefDelete = await doc(db,"posts", postId)
+        deleteDoc(conRefDelete)
+        console.log("success");
+        
+    }
+    return (
+        <div>
+            <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5 mb-10">
+        <form onSubmit={handleUpdatePost}>
+          <input
+            type="text"
+            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500"
+            placeholder="Enter your title"
+            name="title"
+            onChange={(e)=>{setTitle(e.target.value)}}
+          />
+          <input
+            type="text"
+            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500"
+            placeholder="Enter your author"
+            name="author"
+            onChange={(e)=>{setAuthor(e.target.value)}}
+          />
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-medium text-white bg-blue-500 rounded-lg"
+          >
+            Update post
+          </button>
+        </form>
+      </div>
+
+        {/* remove post */}
+      <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5 mb-10">
+        <form onSubmit={handleRemovePost}>
+          <input
+            type="text"
+            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500"
+            placeholder="Enter your id"
+            name="postId"
+            onChange={(e)=>{setPostId(e.target.value)}}
+          />
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-medium text-white bg-red-500 rounded-lg"
+          >
+            Remove post
+          </button>
+        </form>
+      </div>
+
+      {/* posts */}
+      <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5 mb-10">
+        {post.length > 0 && post.map((item)=>(
+            <div key={item.id}>
+                {item.title} - {item.author}
+            </div>
+        ))}
+      </div>
+    </div>
+    );
+};
+
+export default FirebaseApp;
---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVEXybFEGnYmz7QRqkVTTpA2IlJUBaNUE",
+  authDomain: "learn-firebase-26cc4.firebaseapp.com",
+  projectId: "learn-firebase-26cc4",
+  storageBucket: "learn-firebase-26cc4.appspot.com",
+  messagingSenderId: "129150279614",
+  appId: "1:129150279614:web:6cee376b54aad76c33e3ef",
+  measurementId: "G-KNLKKDF1FH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const db = getFirestore(app)
